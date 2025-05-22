85fd58fc25f6257ec0fd86ba72fdb480b999b598
mg0x7BE <108034506+mg0x7BE@users.noreply.github.com>
2025-05-22 16:14:24 +0200

    Add script to change database and column-level collation

---
+++ Collation.sql
@@ -0,0 +1,134 @@
+
+/**********************************************************************************************/
+-- Change database collation
+
+ALTER DATABASE my_database SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+GO
+
+ALTER DATABASE my_database
+    COLLATE Latin1_General_BIN;
+GO
+
+ALTER DATABASE my_database SET MULTI_USER;
+GO
+
+/**********************************************************************************************/
+-- Change collation on column-level
+
+use my_database;
+
+SELECT
+    s.name AS SchemaName,
+    t.name AS TableName,
+    c.name AS ColumnName,
+    ty.name AS DataType,
+    c.max_length AS DataMaxLength,
+    c.collation_name AS Collation,
+    'ALTER TABLE [' + s.name + '].[' + t.name + '] ALTER COLUMN [' + c.name + '] ' +
+    ty.name +
+    CASE
+        WHEN c.max_length = -1 THEN '(MAX)'
+        WHEN ty.name IN ('char', 'varchar', 'nchar', 'nvarchar') THEN '(' + CAST(c.max_length / (CASE WHEN ty.name IN ('nchar', 'nvarchar') THEN 2 ELSE 1 END) AS VARCHAR(10)) + ')'
+        ELSE ''
+        END +
+    ' COLLATE Latin1_General_BIN ' + CASE WHEN c.is_nullable = 0 THEN ' NOT NULL' ELSE ' NULL' END AS AlterScript
+
+FROM sys.tables t
+         INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+         INNER JOIN sys.columns c ON t.object_id = c.object_id
+         INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+WHERE c.collation_name <> 'Latin1_General_BIN'
+  AND t.type = 'U' -- User Table
+  AND ty.is_user_defined <> 1
+ORDER BY 1,2,3
+
+/**********************************************************************************************/
+-- Find object
+
+SELECT
+    o.name AS ObjectName,
+    SCHEMA_NAME(o.schema_id) AS SchemaName,
+    o.type_desc AS ObjectType,
+    o.create_date AS CreationDate,
+    o.modify_date AS ModificationDate,
+    m.definition AS ObjectDefinition
+FROM
+    sys.objects o
+LEFT JOIN
+    sys.sql_modules m ON o.object_id = m.object_id
+WHERE
+    o.name LIKE '%fnMyFunction%'
+  AND o.type IN ('FN', 'IF', 'TF', 'P', 'V', 'U', 'TR')
+ORDER BY
+    o.name;
+
+/**********************************************************************************************/
+-- Find column
+
+SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE COLUMN_NAME = 'MyColumn'
+  AND TABLE_NAME LIKE '%MyTable%';
+
+/**********************************************************************************************/
+-- Change collation on column-level (uncomment sp_executesql section)
+
+use my_database
+
+IF OBJECT_ID('tempdb..#AlterScripts') IS NOT NULL
+    DROP TABLE #AlterScripts;
+
+CREATE TABLE #AlterScripts (
+                               ID INT IDENTITY(1,1),
+                               AlterScript NVARCHAR(MAX)
+);
+
+INSERT INTO #AlterScripts (AlterScript)
+SELECT
+    'ALTER TABLE [' + s.name + '].[' + t.name + '] ALTER COLUMN [' + c.name + '] ' +
+    ty.name +
+    CASE
+        WHEN c.max_length = -1 THEN '(MAX)'
+        WHEN ty.name IN ('char', 'varchar', 'nchar', 'nvarchar') THEN '(' + CAST(c.max_length / (CASE WHEN ty.name IN ('nchar', 'nvarchar') THEN 2 ELSE 1 END) AS VARCHAR(10)) + ')'
+        ELSE ''
+        END +
+    ' COLLATE Latin1_General_BIN ' +
+    CASE WHEN c.is_nullable = 0 THEN ' NOT NULL' ELSE ' NULL' END AS AlterScript
+FROM sys.tables t
+         INNER JOIN sys.schemas s ON t.schema_id = s.schema_id
+         INNER JOIN sys.columns c ON t.object_id = c.object_id
+         INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id
+WHERE c.collation_name <> 'Latin1_General_BIN'
+  AND t.type = 'U' -- User Table
+  AND ty.is_user_defined <> 1
+ORDER BY s.name, t.name, c.name;
+
+
+DECLARE @CurrentScript NVARCHAR(MAX);
+DECLARE @CurrentID INT;
+DECLARE @MaxID INT;
+
+
+SELECT @MaxID = MAX(ID) FROM #AlterScripts;
+SET @CurrentID = 1;
+
+WHILE @CurrentID <= @MaxID
+    BEGIN
+
+        SELECT @CurrentScript = AlterScript
+        FROM #AlterScripts
+        WHERE ID = @CurrentID;
+
+        BEGIN TRY
+            -- EXEC sp_executesql @CurrentScript;
+            -- PRINT 'OK: ' + @CurrentScript;
+        END TRY
+        BEGIN CATCH
+            PRINT 'Error in: ' + @CurrentScript;
+            PRINT 'Error: ' + ERROR_MESSAGE();
+        END CATCH
+
+        SET @CurrentID = @CurrentID + 1;
+    END;
+
+DROP TABLE #AlterScripts;
