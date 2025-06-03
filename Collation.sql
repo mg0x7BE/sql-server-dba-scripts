98ee378a73375779adfed474c4ef4bec4821c31c
mg0x7BE <108034506+mg0x7BE@users.noreply.github.com>
2025-06-03 10:37:18 +0200

    Add script to check and report database and column-level collation

--- Collation.sql
+++ Collation.sql
@@ -1,5 +1,63 @@
 
 /**********************************************************************************************/
+-- Check database-level and column-level collation for the entire instance
+
+IF OBJECT_ID('tempdb..#CollationStats') IS NOT NULL
+    DROP TABLE #CollationStats;
+
+CREATE TABLE #CollationStats (
+                                 ServerName NVARCHAR(128),
+                                 DatabaseName NVARCHAR(128),
+                                 Collation NVARCHAR(128),
+                                 CollationCount INT,
+                                 SourceType NVARCHAR(50)
+);
+INSERT INTO #CollationStats (ServerName, DatabaseName, Collation, CollationCount, SourceType)
+SELECT
+            @@SERVERNAME AS ServerName,
+            d.name AS DatabaseName,
+            d.collation_name AS Collation,
+            0 AS CollationCount,
+            'Database' AS SourceType
+FROM sys.databases d
+WHERE d.name not in ('tempdb','msdb')
+GROUP BY d.name, d.collation_name;
+
+DECLARE @SQL NVARCHAR(MAX) = '';
+SELECT @SQL = @SQL +
+              'USE [' + name + ']; ' +
+              'INSERT INTO #CollationStats (ServerName, DatabaseName, Collation, CollationCount, SourceType) ' +
+              'SELECT ' +
+              '@@SERVERNAME AS ServerName, ' +
+              '''' + name + ''' AS DatabaseName, ' +
+              'c.collation_name AS Collation, ' +
+              'COUNT(*) AS CollationCount, ' +
+              '''Columns'' AS SourceType ' +
+              'FROM sys.tables t ' +
+              'INNER JOIN sys.schemas s ON t.schema_id = s.schema_id ' +
+              'INNER JOIN sys.columns c ON t.object_id = c.object_id ' +
+              'INNER JOIN sys.types ty ON c.user_type_id = ty.user_type_id ' +
+              'WHERE t.type = ''U'' ' +
+              'AND ty.is_user_defined <> 1 ' +
+              'AND c.collation_name IS NOT NULL ' +
+              'GROUP BY c.collation_name; '
+FROM sys.databases
+WHERE state = 0 and name not in ('tempdb','msdb');
+
+EXEC sp_executesql @SQL;
+
+SELECT
+    ServerName,
+    DatabaseName,
+    SourceType,
+    CASE WHEN (SourceType = 'Database') THEN 'Database' ELSE CONVERT(nvarchar(255), CollationCount) END as CollationCount,
+    Collation
+FROM #CollationStats
+ORDER BY 1,2,3 desc,5
+DROP TABLE #CollationStats;
+
+
+/**********************************************************************************************/
 -- Change database collation
 
 ALTER DATABASE my_database SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
